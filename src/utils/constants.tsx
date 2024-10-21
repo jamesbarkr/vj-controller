651ee96d1574c61500f2580d34e950974ba7afc6
Aspen Santos <aspen.santos@uwaterloo.ca>
2024-10-20 20:40:29 -0700

    Add animation utilities to enable state based keyframe animations by interpolating between specified states.
    Add a tube geometry with a path specified by a sin curve.
    
    Add a playground scene for myself, currently testing textures, animations and reflections
    
    Also:
    - Add an example of how animation utilities could be used
    - directory clean up

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Cube from "../components/Cube";
 import SpinningCube from "../components/SpinningCube";
+import AspenPlaygroundScene from "../components/scenes/AspenPlaygroundScene";
 
 export const LOCAL_VIZ_KEY = "VJ-Controller__visualization";
 export const LOCAL_SCALE_KEY = "VJ-Controller__scale";
@@ -9,9 +10,11 @@ export const LOCAL_CAMERA_CONTROLS_KEY = "VJ-Controller__camera_controls";
 export enum Visualization {
   CUBE = "cube",
   SPINNING_CUBE = "spinningCube",
+  ASPEN_PLAYGROUND = "aspenPlayground"
 }
 
 export const VisualizationMap: Record<string, ReactNode> = {
   [Visualization.CUBE]: <Cube />,
   [Visualization.SPINNING_CUBE]: <SpinningCube />,
+  [Visualization.ASPEN_PLAYGROUND]: <AspenPlaygroundScene/>
 };
