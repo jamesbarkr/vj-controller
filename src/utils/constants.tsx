20abdb8b6271183d3d8224cfe20fe4133b43d137
Aspen Santos <aspensantos99@gmail.com>
2024-10-30 21:00:14 -0700

    Lasers!
    - add pre-preprocessing package
    - scene with tubes to look like lasers
    - bloom effects

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Cube from "../components/Cube";
 import SpinningCube from "../components/SpinningCube";
 import AspenPlaygroundScene from "../components/scenes/AspenPlaygroundScene";
+import LaserScene from "../components/scenes/LaserScene";
 
 export const LOCAL_VIZ_KEY = "VJ-Controller__visualization";
 export const LOCAL_SCALE_KEY = "VJ-Controller__scale";
@@ -10,11 +11,13 @@ export const LOCAL_CAMERA_CONTROLS_KEY = "VJ-Controller__camera_controls";
 export enum Visualization {
   CUBE = "cube",
   SPINNING_CUBE = "spinningCube",
-  ASPEN_PLAYGROUND = "aspenPlayground"
+  ASPEN_PLAYGROUND = "aspenPlayground",
+  LASERS = "lasers"
 }
 
 export const VisualizationMap: Record<string, ReactNode> = {
   [Visualization.CUBE]: <Cube />,
   [Visualization.SPINNING_CUBE]: <SpinningCube />,
-  [Visualization.ASPEN_PLAYGROUND]: <AspenPlaygroundScene/>
+  [Visualization.ASPEN_PLAYGROUND]: <AspenPlaygroundScene/>,
+  [Visualization.LASERS]: <LaserScene/>
 };
