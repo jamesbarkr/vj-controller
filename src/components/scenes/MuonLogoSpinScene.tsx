c29d418924d8b393534ae5defcd41d2bed2ccea6
James Barker <james@propelauth.com>
2024-11-05 00:32:04 -0800

    feat: reset spin hidden by glitch

--- src/components/scenes/MuonLogoSpinScene.tsx
+++ src/components/scenes/MuonLogoSpinScene.tsx
@@ -7,7 +7,7 @@ import {
 } from "@react-three/postprocessing";
 import { BlendFunction, GlitchMode } from "postprocessing";
 import { useFrame, useLoader } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
+import { useMemo, useRef, useState } from "react";
 import {
   Clock,
   DoubleSide,
@@ -20,6 +20,7 @@ import { SVGLoader } from "three/examples/jsm/Addons.js";
 
 const MuonLogoSpinScene = () => {
   const muonLogoRef = useRef<Mesh>(null!);
+  const [goWild, setGoWild] = useState(false);
   const data = useLoader(SVGLoader, "/src/assets/muonCassette.svg");
   const shapes = data.paths.flatMap((path) => SVGLoader.createShapes(path));
   const glitchTexture = useLoader(TextureLoader, "/src/assets/3.png");
@@ -47,7 +48,15 @@ const MuonLogoSpinScene = () => {
 
   useFrame(() => {
     const elapsedTime = clock.getElapsedTime();
-    muonLogoRef.current.rotation.y = elapsedTime * Math.PI * 0.25;
+    muonLogoRef.current.rotation.y =
+      Math.PI * -0.45 + elapsedTime * Math.PI * 0.25;
+  });
+
+  useFrame(() => {
+    if (muonLogoRef.current.rotation.y >= Math.PI / 2.1 && !goWild) {
+      setGoWild(true);
+      setTimeout(() => setGoWild(false), 100);
+    }
   });
 
   return (
@@ -68,9 +77,9 @@ const MuonLogoSpinScene = () => {
           delay={new Vector2(0.1, 3)} // min and max glitch delay
           duration={new Vector2(0.1, 0.3)} // min and max glitch duration
           strength={new Vector2(0.1, 0.5)} // min and max glitch strength
-          mode={GlitchMode.SPORADIC} // glitch mode
+          mode={goWild ? GlitchMode.CONSTANT_WILD : GlitchMode.SPORADIC} // glitch mode
           active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
-          ratio={0.75} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+          ratio={0.25} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
         />
       </EffectComposer>
       {muonLogo}
