52908a7345fa9b0ce02b54bba9601251a1fbe6c4
James Barker <james@propelauth.com>
2024-11-11 19:06:48 -0800

    feat: updated muon tile pattern

--- src/components/pixi/scenes/MuonTiledScene.tsx
+++ src/components/pixi/scenes/MuonTiledScene.tsx
@@ -14,7 +14,7 @@ const MuonTiledScene = () => {
 
   useTick((delta) => {
     if (delta) {
-      // spriteRef.current.tilePosition.x += SPRITE_SPEED * delta;
+      spriteRef.current.tilePosition.x -= SPRITE_SPEED * delta;
       spriteRef.current.tilePosition.y += SPRITE_SPEED * delta;
     }
   });
@@ -28,15 +28,13 @@ const MuonTiledScene = () => {
       <Filters colorOverlayFilter={{ color: 0xffffff, alpha: 1 }}>
         <TilingSprite
           ref={spriteRef}
-          image="/src/assets/muonCassetteSpaced.svg"
+          image="/src/assets/muonBendySpaced.svg"
           width={windowDiag}
           height={windowDiag}
           anchor={[0.5, 0.5]}
           position={[window.innerWidth / 2, window.innerHeight / 2]}
           rotation={Math.PI / -4}
           tilePosition={[0, 0]}
-          // tileScale={[0.5, 0.5]}
-          clampMargin={-0.5}
         />
       </Filters>
     </>
